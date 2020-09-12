7ff9bf7c22b1d22d6bbd65e56ab6ecf92ec465fb
昌维 <867597730@qq.com>
2020-09-13 03:37:08 +0800

    no message

--- src/me/chagnwei/leetcode/romanToInt/Solution.java
+++ src/me/chagnwei/leetcode/romanToInt/Solution.java
@@ -7,58 +7,36 @@ package me.chagnwei.leetcode.romanToInt;
  * @description
  */
 public class Solution {
-    private static enum RomanEnum {
-        /**
-         * 来源：力扣（LeetCode）
-         * 链接：https://leetcode-cn.com/problems/roman-to-integer
-         * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-         */
-        I('I', 1),
-        V('V', 5),
-        X('X', 10),
-        L('L', 50),
-        C('C', 100),
-        D('D', 500),
-        M('M', 1000);
-
-        private char c;
-        private int v;
-
-        RomanEnum(char c, int v) {
-            this.c = c;
-            this.v = v;
-        }
-
-        public char getC() {
-            return c;
-        }
-
-        public int getV() {
-            return v;
-        }
+    public static void main(String[] args) {
+        System.out.println(new Solution().romanToInt("III"));
+        System.out.println(new Solution().romanToInt("IV"));
+        System.out.println(new Solution().romanToInt("IX"));
+        System.out.println(new Solution().romanToInt("LVIII"));
+        System.out.println(new Solution().romanToInt("MCMXCIV"));
     }
+
     public int romanToInt(String s) {
         int sum = 0;
         s += 0;
         char[] charArray = s.toCharArray();
         for (int i = 0; i < charArray.length; i++) {
             char c = charArray[i];
-            if (c == 'I' && charArray[i+1] == 'V') {
+            if (c == 'I' && charArray[i + 1] == 'V') {
                 sum += 4;
                 ++i;
-            } else if (c == 'I' && charArray[i+1] == 'X') {
+            } else if (c == 'I' && charArray[i + 1] == 'X') {
                 sum += 9;
                 ++i;
-            } else if (c == 'X' && charArray[i+1] == 'L') {
+            } else if (c == 'X' && charArray[i + 1] == 'L') {
                 sum += 40;
                 ++i;
-            } else if (c == 'X' && charArray[i+1] == 'C') {
+            } else if (c == 'X' && charArray[i + 1] == 'C') {
                 sum += 90;
                 ++i;
-            } else if (c == 'C' && charArray[i+1] == 'D') {
+            } else if (c == 'C' && charArray[i + 1] == 'D') {
                 sum += 400;
                 ++i;
-            } else if (c == 'C' && charArray[i+1] == 'M') {
+            } else if (c == 'C' && charArray[i + 1] == 'M') {
                 sum += 900;
                 ++i;
             } else {
@@ -101,11 +79,34 @@ public class Solution {
         return sum;
     }
 
-    public static void main(String[] args) {
-        System.out.println(new Solution().romanToInt("III"));
-        System.out.println(new Solution().romanToInt("IV"));
-        System.out.println(new Solution().romanToInt("IX"));
-        System.out.println(new Solution().romanToInt("LVIII"));
-        System.out.println(new Solution().romanToInt("MCMXCIV"));
+    private enum RomanEnum {
+        /**
+         * 来源：力扣（LeetCode）
+         * 链接：https://leetcode-cn.com/problems/roman-to-integer
+         * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+         */
+        I('I', 1),
+        V('V', 5),
+        X('X', 10),
+        L('L', 50),
+        C('C', 100),
+        D('D', 500),
+        M('M', 1000);
+
+        private char c;
+        private int v;
+
+        RomanEnum(char c, int v) {
+            this.c = c;
+            this.v = v;
+        }
+
+        public char getC() {
+            return c;
+        }
+
+        public int getV() {
+            return v;
+        }
     }
 }
